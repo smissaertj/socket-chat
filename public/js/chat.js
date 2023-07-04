3bf3d989989407a119146d94eff1de9259b20cfb
Joeri Smissaert <smissaertj@gmail.com>
2023-07-04 18:08:57 +0400

    Added .eslint, full-duplex communication example

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+/* eslint-env browser */
+/* eslint-disable no-undef */
+const socket = io();
+
+socket.on( "countUpdated", ( count ) => {
+  console.log( "The count has been updated!", count );
+} );
+
+const incrementCount = document.querySelector( "#increment" );
+incrementCount.addEventListener( "click", ()=> {
+  console.log( "Clicked" );
+  socket.emit( "increment" );
+} );
--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-const path = require('node:path')
-const http = require('node:http')
-const express = require('express');
-const socketio = require('socket.io')
+const path = require( "node:path" );
+const http = require( "node:http" );
+const express = require( "express" );
+const socketio = require( "socket.io" );
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const server = http.createServer( app );
+const io = socketio( server );
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join( __dirname, "../public" );
 
-app.use(express.static(publicDirectoryPath))
+app.use( express.static( publicDirectoryPath ) );
 
-io.on('connection', () => {
-  console.log('New WebSocket connection');
-})
+let count = 0;
 
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-})
+io.on( "connection", ( socket ) => {
+  console.log( "New WebSocket connection" );
+  socket.emit( "countUpdated", count );
+  socket.on( "increment", () => {
+    count++;
+    io.emit( "countUpdated", count );
+  } );
+} ); 
+
+server.listen( port, () => {
+  console.log( `Server is up on port ${port}` );
+} );
